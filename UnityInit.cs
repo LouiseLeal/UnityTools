37ce39c3433b1f461c94682d9429b071a45b2749
Louise leal <lcloudine@gmail.com>
2020-09-30 14:23:06 -0400

    Add script to create git tracable empty folders

---
+++ UnityInit.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class UnityInit : MonoBehaviour
+{
+
+    [MenuItem("UnityInit/Create Folders ")]
+
+    static void CreateInitalFolders()
+    {
+        //Get the path of the Game data folder
+        var m_Path = Application.dataPath;
+
+        //Output the Game data path to the console
+        var folder = Directory.CreateDirectory(m_Path+"/Scripts");
+        Debug.Log("folder : " + folder.FullName);
+        //Add .gitkeep to git track the empty folders
+        var file = File.Create(folder.FullName+"/.gitkeep");
+
+        folder = Directory.CreateDirectory(m_Path + "/Prefabs");
+        Debug.Log("folder : " + folder.FullName);
+        file = File.Create(folder.FullName + "/.gitkeep");
+
+        folder = Directory.CreateDirectory(m_Path + "/Materials");
+        Debug.Log("folder : " + folder.FullName);
+        file = File.Create(folder.FullName + "/.gitkeep");
+
+        folder = Directory.CreateDirectory(m_Path + "/Textures");
+        Debug.Log("folder : " + folder.FullName);
+        file = File.Create(folder.FullName + "/.gitkeep");
+
+        folder = Directory.CreateDirectory(m_Path + "/Material");
+        Debug.Log("folder : " + folder.FullName);
+        file = File.Create(folder.FullName + "/.gitkeep");
+
+        folder = Directory.CreateDirectory(m_Path + "/Plugins");
+        Debug.Log("folder : " + folder.FullName);
+        file = File.Create(folder.FullName + "/.gitkeep");
+    }
+}
